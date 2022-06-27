9a9f2d183cded9dc32b4f3642254cd03bcc77be0
unknown <jadentr.44@gmail.com>
2022-06-27 16:57:14 -0500

    working password genarator that prints in console

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", generatePassword);
+var passwordText = document.querySelector("#password");
+
 const upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 const lowerChar = ["b","b","d","d","e","f","g","h","i","j","k","l","l","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 const numbers = ["1","2","3","4","5","6","7","8","9"]
 const specialChar =["!","@","#","$","%","^","&","*"]
 
 
-function askQuestions(){
+function generatePassword(){
   let passwordArr = []
   let usableChar = []
 
@@ -24,7 +28,20 @@ if(useUpper){usableChar = usableChar.concat(upperChar)}
 if(useLower){usableChar = usableChar.concat(lowerChar)}
 if(useNumber){usableChar = usableChar.concat(numbers)}
 if(useSpecial){usableChar = usableChar.concat(specialChar)}
-console.log(usableChar)
+
+for(let i =0; i < passwordLength;i++){
+  let randomIndex =  Math.floor(Math.random() * usableChar.length)
+  let randomChar = usableChar[randomIndex]
+  if(passwordArr.length === 0){//if the password array is empty
+    passwordArr.unshift(randomChar)
+  }else if(passwordArr.length-1 === randomIndex){//if its trying to add it to the end
+    passwordArr.push(randomChar)
+  }
+  else{//putting the random character in the random index
+    passwordArr.splice(randomIndex,0,randomChar)
+  }
+}
+console.log(passwordArr.join(""))
 //functions used
 function promptChoices(e){
     let answer;
@@ -48,4 +65,4 @@ function askLength(){
 }
 
 
-askQuestions()
+generatePassword()
