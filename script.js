bbebf20d2353cce8b1c5846076ed2f42f3162952
unknown <jadentr.44@gmail.com>
2022-06-27 16:30:29 -0500

    can ask and check length, as well as formatting the code

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-let passwordArr = []
-
-let length;
-
 const upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 const lowerChar = ["b","b","d","d","e","f","g","h","i","j","k","l","l","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 const numbers = ["1","2","3","4","5","6","7","8","9"]
 const specialChar =["!","@","#","$","%","^","&","*"]
 
-const usableChar = []
+
 function askQuestions(){
+  let passwordArr = []
+
+  let length;
 let useUpper = promptChoices("Uppercase characters")
 let useLower = promptChoices("lowercase characters")
 let useNumber = promptChoices("numbers")
@@ -17,6 +16,10 @@ if(!useUpper && !useLower && !useNumber && !useSpecial){
   alert("you must select 1")
   askQuestions()
 }
+let passwordLength = askLength()
+
+if(useUpper){}
+//functions used
 function promptChoices(e){
     let answer;
     askAndCheck()
@@ -24,10 +27,19 @@ function promptChoices(e){
   else{return false}
   function askAndCheck(){
     console.log("here too")
-     answer = prompt(`Would you like ${e} in your password?'Y' for yes or 'N' for no`);
+     answer = prompt(`Would you like ${e} in your password?\n'Y' for yes\n'N' for no`);
      answer = answer.toUpperCase()
     if(answer !=="Y" && answer !== "N"){ askAndCheck()}
   }
 }
 }
+
+function askLength(){
+  let answerLength = prompt("enter a password length between 8-128")
+  if(answerLength >= 8 && answerLength<= 128){
+    return answerLength
+  }else{askLength()}
+}
+
+
 askQuestions()
